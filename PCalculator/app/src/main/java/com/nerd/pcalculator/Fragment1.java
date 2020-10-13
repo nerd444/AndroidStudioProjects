a412586d836984d64c09c7109c492346dd1efeb0
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-10-13 22:20:55 +0900

    add

--- PCalculator/app/src/main/java/com/nerd/pcalculator/Fragment1.java
+++ PCalculator/app/src/main/java/com/nerd/pcalculator/Fragment1.java
@@ -7,58 +7,65 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Fragment1 extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Fragment1() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment1 newInstance(String param1, String param2) {
-        Fragment1 fragment = new Fragment1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    EditText edit_text;
+    TextView txt_result;
+    Button btn_clear;
+    Button btn_record;
+
+    Button btn_division;
+    Button btn_multi;
+    Button btn_minus;
+    Button btn_plus;
+
+    Button btn_cancle;
+    Button btn_result;
+
+    Button btn_0;
+    Button btn_1;
+    Button btn_2;
+    Button btn_3;
+    Button btn_4;
+    Button btn_5;
+    Button btn_6;
+    Button btn_7;
+    Button btn_8;
+    Button btn_9;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+        edit_text = view.findViewById(R.id.edit_text);
+        txt_result = view.findViewById(R.id.txt_result);
+        btn_clear = view.findViewById(R.id.btn_clear);
+        btn_record = view.findViewById(R.id.btn_record);
+        btn_division = view.findViewById(R.id.btn_division);
+        btn_multi = view.findViewById(R.id.btn_multi);
+        btn_minus = view.findViewById(R.id.btn_minus);
+        btn_plus = view.findViewById(R.id.btn_plus);
+        btn_cancle = view.findViewById(R.id.btn_cancle);
+        btn_result = view.findViewById(R.id.btn_result);
+        btn_0 = view.findViewById(R.id.btn_0);
+        btn_1 = view.findViewById(R.id.btn_1);
+        btn_2 = view.findViewById(R.id.btn_2);
+        btn_3 = view.findViewById(R.id.btn_3);
+        btn_4 = view.findViewById(R.id.btn_4);
+        btn_5 = view.findViewById(R.id.btn_5);
+        btn_6 = view.findViewById(R.id.btn_6);
+        btn_7 = view.findViewById(R.id.btn_7);
+        btn_8 = view.findViewById(R.id.btn_8);
+        btn_9 = view.findViewById(R.id.btn_9);
+
+        return view;
     }
 }
