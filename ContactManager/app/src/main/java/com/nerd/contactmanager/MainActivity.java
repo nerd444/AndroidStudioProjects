8fc3a2953c35588c21f731db7fc5a524ceff2125
nerd444 <tkdwls0712@naver.com>
2020-10-16 17:50:11 +0900

    add

--- ContactManager/app/src/main/java/com/nerd/contactmanager/MainActivity.java
+++ ContactManager/app/src/main/java/com/nerd/contactmanager/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.nerd.contactmanager.adapter.RecyclerViewAdapter;
 import com.nerd.contactmanager.data.DatabaseHandler;
@@ -31,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
     int id;
     AlertDialog dialog;
+    public static Context context;
 
     RecyclerView recyclerView;      // 메인 화면에 있는 리사이클러 뷰    (이거부터아래 세개는 세트임.)
     RecyclerViewAdapter recyclerViewAdapter;        // 우리가 만든, 하나의 셀을 연결시키는 어댑터
@@ -46,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+        createPopupDialog();
 
     }
 
@@ -91,6 +96,58 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createPopupDialog(){
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+        View alertView = getLayoutInflater().inflate(R.layout.activity_update_contact,null);
+
+        editName = findViewById(R.id.editName);
+        editPhone = findViewById(R.id.editPhone);
+        btnSave = findViewById(R.id.btnSave);
+        btnCancle = findViewById(R.id.btnCancle);
+
+        // 어댑터에서, 유저가 클릭한 경우, 데이터를 받아온다.
+        id = getIntent().getIntExtra("id",-1);
+        String n = getIntent().getStringExtra("name");
+        String p = getIntent().getStringExtra("phone");
+        // 위에서 받아온 데이터를, 에디트 텍스트에 보여준다.
+        editName.setText(n);
+        editPhone.setText(p);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 데이터베이스에 업데이트.
+                // 유저가 변경했을 수 있는 이름과 폰번을, 에디트 텍스트에서 가져온다.
+                String name = editName.getText().toString().trim();
+                String phone = editPhone.getText().toString().trim();
+
+                // 디비핸들러 클래스를 통해서 업데이트
+                DatabaseHandler db = new DatabaseHandler(MainActivity.this);
+                Contact contact = new Contact(id, name, phone);
+                db.updateContact(contact);
+                // 토스트를 보여준다.
+                Toast.makeText(MainActivity.this, "수정되었습니다.",Toast.LENGTH_SHORT).show();
+                // 이 액티비티 종료.
+                finish();
+
+                dialog.cancel();
+            }
+        });
+
+        btnCancle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+        alert.setView(alertView);
+        alert.setCancelable(false);
+
+        dialog = alert.create();
+        dialog.show();
     }
 
 }
